be074f7e7a7c47c24d522b5c3093fc55a4c4a848
王语涵 <wyh574309436@163.com>
2020-04-10 17:28:58 +0800

    1、上传新的网管服务基本框架

---
+++ banshee-gateway/src/main/java/com/gundam/gateway/BansheeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gundam.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BansheeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BansheeGatewayApplication.class, args);
+    }
+
+}
---
+++ banshee-gateway/src/test/java/com/gundam/gateway/BansheeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gundam.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BansheeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
