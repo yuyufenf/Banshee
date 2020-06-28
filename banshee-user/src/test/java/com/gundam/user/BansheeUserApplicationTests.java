f9d08741dca119c355f154bc79439b47f32e7dea
王语涵 <wyh574309436@163.com>
2020-06-28 17:08:23 +0800

    1、添加用户模块初始框架 2、替换修改各服务配置文件

---
+++ banshee-user/src/test/java/com/gundam/user/BansheeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gundam.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BansheeUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
